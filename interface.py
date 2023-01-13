6f657db73597394db1e8f62c31f271f50f835e7f
Renan Almeida <101940943+RenanMaestrya@users.noreply.github.com>
2023-01-13 20:10:38 -0300

    update: interface menu

--- interface.py
+++ interface.py
@@ -7,6 +7,7 @@ from person_with_reservation import Operator, Passenger
 from reservation import Reservation
 from seat import Seat
 
+from datetime import datetime
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -63,12 +64,11 @@ def menuAdmin():
     print("|         1. Cadastrar Cidade                 |")
     print("|         2. Cadastrar Aeroporto              |")
     print("|         3. Cadastrar Tripulação             |")
-    print("|         4. Cadastrar Lista de Assentos      |")
-    print("|         5. Cadastrar Voô                    |")
-    print("|         6. Cadastrar Operador               |")
-    print("|         7. Sair                             |")
+    print("|         4. Cadastrar Voô                    |")
+    print("|         5. Cadastrar Operador               |")
+    print("|         6. Sair                             |")
     print("===============================================")
-    choice = input("Entre com sua escolha [1-7]: ")
+    choice = input("Entre com sua escolha [1-6]: ")
 
     
     while True:
@@ -79,26 +79,29 @@ def menuAdmin():
             
         elif choice == '2':
             clear()
+            CadastroAeroporto()
+            break
             
         elif choice == '3':
             clear()
-            
+            CadastroTripulacao()
+            break
+
         elif choice == '4':
             clear()
+            CadastroVoo()
+            break
 
         elif choice == '5':
             clear()
-
-        elif choice == '6':
-            clear()
             
-        elif choice == '7':
+        elif choice == '6':
             clear()
             print("Saindo do programa...")
             break
         else:
             clear()
-            print("Opção inválida. Escolha novamente.")
+            Iniciar()
 
     
 
@@ -137,39 +140,111 @@ def menuUser():
             clear()
             print("Opção inválida. Escolha novamente.")
 
-def CadastroCidade():
-    clear()
-    print("===============================================")
-    print("|               Cadastrar Cidade              |")
-    print("===============================================")
-    pais = input("              País: ")
-    estado = input("              Estado: ")
-    cidade = input("              Cidade: ")
 
-    instance = City(pais, estado, cidade)
-    City_list.append(instance)
-    print("===============================================")
+
+
+
+
+Airport_list = []
+City_list = []
+Crew_list = []
+Seat_list = []
+Flight_list = []
+
+
+def CadastroCidade():
+    while True:
+        clear()
+        print("===============================================")
+        print("|               Cadastrar Cidade              |")
+        print("===============================================")
+        pais = input("              País: ")
+        estado = input("              Estado: ")
+        cidade = input("              Cidade: ")
+
+        instance = City(pais, estado, cidade)
+        City_list.append(instance)
+        print("===============================================")
+        next = input('Continuar cadastrando? (s/n):  ')
+        if next == 's':
+            continue
+        else:
+            clear()
+            menuAdmin()
+            break
 
 def CadastroAeroporto():
-    clear()
-    print("===============================================")
-    print("|             Cadastrar Aeroporto             |")
-    print("===============================================")
-    nome = input("              Nome: ")
-    decolagens = int(input("              Decolagens suportadas: "))
-    
+    while True:
+        clear()
+        print("===============================================")
+        print("|             Cadastrar Aeroporto             |")
+        print("===============================================")
+        nome = input("              Nome: ")
+        decolagens = int(input("              Decolagens suportadas: "))
+        
 
-    instance = Airport(City_list[0], nome, decolagens)
-    Airport_list.append(instance)
+        instance = Airport(City_list[0], nome, decolagens)
+        Airport_list.append(instance)
 
-    print("===============================================")
+        print("===============================================")
 
-    if instance.name != '' and instance.max_departures != '':
-        print('Aeroporto cadastrado com sucesso!')
-    else:
-        print('Aeroporto não cadastrado!')
+        if instance.name != '' and instance.max_departures != '':
+            print('Aeroporto cadastrado com sucesso!')
+        else:
+            print('Aeroporto não cadastrado!')
+        
+        next = input('Continuar cadastrando? (s/n):  ')
+        if next == 's':
+            continue
+        else:
+            clear()
+            menuAdmin()
+            break
 
+def CadastroTripulacao():
+    while True:
+        clear() 
+        print("===============================================")
+        print("|             Cadastrar Tripulação            |")
+        print("===============================================")
+        nome = input("              Nome: ")
+        funcao = input("              Função: ")
+
+        instance = Crew(nome, funcao)
+        Crew_list.append(instance)
+        print("===============================================")
+        next = input('Continuar cadastrando? (s/n):  ')
+        if next == 's':
+            continue
+        else:
+            clear()
+            menuAdmin()
+            break
+
+
+def CadastroVoo():
+    while True:
+        clear() 
+        print("===============================================")
+        print("|                 Cadastrar Voô               |")
+        print("===============================================")
+        tipo = input("              Tipo(Nacional|Internacional): ")
+        assentos = int(input("  Quantidade de assentos: "))
+        for i in range(0, assentos):
+            assento = input("              ID do assento: ")
+            Seat_list.append(assento)
+
+        Flight_list.append(Flight(tipo, Airport_list[0], Airport_list[1]), datetime.now().time(), datetime.now().date(), Seat_list, Crew_list)
+        print("===============================================")
+        next = input('Continuar cadastrando? (s/n):  ')
+        if next == 's':
+            continue
+        else:
+            clear()
+            menuAdmin()
+            break
+
+# Iniciar()
 # CadastroCidade()
-Airport_list = []
-City_list = [City('dasd', 'dasijdias', 'duas')]
-CadastroAeroporto()
+# CadastroAeroporto()
+# CadastroTripulacao()
