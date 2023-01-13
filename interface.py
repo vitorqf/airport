9d01cfce96da08011f55b78c7fdfba880694f906
Renan Almeida <101940943+RenanMaestrya@users.noreply.github.com>
2023-01-13 12:08:03 -0300

    Feat: menu start

--- interface.py
+++ interface.py
@@ -3,17 +3,46 @@ import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def Iniciar():
+    clear()
+    print("===============================================")
+    print("|              Admin ou Usuario              |")
+    print("===============================================")
+    print("| Opções:                                    |")
+    print("|                 1. Admin                   |")
+    print("|                 2. Usuario                   |")
+    print("|                 3. Sair                    |")
+    print("===============================================")
+    choice = input("Entre com sua escolha [1-3]: ")
+    return choice
+
+while True:
+    choice = Iniciar()
+    if choice == '1':
+        clear()
+        
+    elif choice == '2':
+        clear()
+        
+    elif choice == '3':
+        clear()
+        print("Saindo do programa...")
+        break
+    else:
+        clear()
+        print("Opção inválida. Escolha novamente.")
+
 def menu():
     clear()
     print("===============================================")
     print("|                    MENU                    |")
     print("===============================================")
     print("| Opções:                                    |")
-    print("|        1. opcao                            |")
-    print("|        2. opcao                            |")
-    print("|        3. opcao                            |")
-    print("|        4. opcao                            |")
-    print("|        5. Sair                             |")
+    print("|                 1. opcao                   |")
+    print("|                 2. opcao                   |")
+    print("|                 3. opcao                   |")
+    print("|                 4. opcao                   |")
+    print("|                 5. Sair                    |")
     print("===============================================")
     choice = input("Entre com sua escolha [1-5]: ")
     return choice
