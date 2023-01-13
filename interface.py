e640d9e29e7f4621b58ee5e73ebb4627975f4056
Renan Almeida <101940943+RenanMaestrya@users.noreply.github.com>
2023-01-13 13:06:09 -0300

    feat menuAdmin menuUser

--- interface.py
+++ interface.py
@@ -3,36 +3,7 @@ import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def Iniciar():
-    clear()
-    print("===============================================")
-    print("|              Admin ou Usuario              |")
-    print("===============================================")
-    print("| Opções:                                    |")
-    print("|                 1. Admin                   |")
-    print("|                 2. Usuario                   |")
-    print("|                 3. Sair                    |")
-    print("===============================================")
-    choice = input("Entre com sua escolha [1-3]: ")
-    return choice
-
-while True:
-    choice = Iniciar()
-    if choice == '1':
-        clear()
-        
-    elif choice == '2':
-        clear()
-        
-    elif choice == '3':
-        clear()
-        print("Saindo do programa...")
-        break
-    else:
-        clear()
-        print("Opção inválida. Escolha novamente.")
-
-def menu():
+def menuAdmin():
     clear()
     print("===============================================")
     print("|                    MENU                    |")
@@ -48,7 +19,7 @@ def menu():
     return choice
 
 while True:
-    choice = menu()
+    choice = menuAdmin()
     if choice == '1':
         clear()
         
@@ -62,6 +33,80 @@ while True:
         clear()
         
     elif choice == '5':
+        clear()
+        print("Saindo do programa...")
+        break
+    else:
+        clear()
+        print("Opção inválida. Escolha novamente.")
+
+
+
+
+
+def menuUser():
+    clear()
+    print("===============================================")
+    print("|                    MENU                    |")
+    print("===============================================")
+    print("| Opções:                                    |")
+    print("|                 1. opcao                   |")
+    print("|                 2. opcao                   |")
+    print("|                 3. opcao                   |")
+    print("|                 4. opcao                   |")
+    print("|                 5. Sair                    |")
+    print("===============================================")
+    choice = input("Entre com sua escolha [1-5]: ")
+    return choice
+
+while True:
+    choice = menuUser()
+    if choice == '1':
+        clear()
+        
+    elif choice == '2':
+        clear()
+        
+    elif choice == '3':
+        clear()
+        
+    elif choice == '4':
+        clear()
+        
+    elif choice == '5':
+        clear()
+        print("Saindo do programa...")
+        break
+    else:
+        clear()
+        print("Opção inválida. Escolha novamente.")
+
+
+
+def Iniciar():
+    clear()
+    print("===============================================")
+    print("|              Admin ou Usuario              |")
+    print("===============================================")
+    print("| Opções:                                    |")
+    print("|                 1. Admin                   |")
+    print("|                 2. Usuario                 |")
+    print("|                 3. Sair                    |")
+    print("===============================================")
+    choice = input("Entre com sua escolha [1-3]: ")
+    return choice
+
+while True:
+    choice = Iniciar()
+    if choice == '1':
+        clear()
+        menuAdmin()
+        
+    elif choice == '2':
+        clear()
+        menuUser()
+        
+    elif choice == '3':
         clear()
         print("Saindo do programa...")
         break
